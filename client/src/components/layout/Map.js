71cf9ec8b2ae44629c52c9069399cc158a923bbd
Nikola <77243263+PancakeB0Y@users.noreply.github.com>
2023-02-14 08:54:47 +0200

    base map

--- client/src/components/layout/Map.js
+++ client/src/components/layout/Map.js
@@ -1,26 +1,26 @@
-import React from 'react';
+import * as React from 'react';
 import { Link, Navigate } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
+import Map from 'react-map-gl';
+
 const MapPage = ({ isAuthenticated }) => {
   return (
-    <section className="landing">
-      <div className="dark-overlay">
-        <div className="landing-inner">
-          <h1 className="x-large">map</h1>
-          <p className="lead">map</p>
-          <div className="buttons">
-            <Link to="/register" className="btn btn-primary">
-              map
-            </Link>
-            <Link to="/login" className="btn btn-light">
-              map
-            </Link>
-          </div>
-        </div>
+    <>
+      <div style={{}}>
+        <Map
+          initialViewState={{
+            longitude: 23.3219,
+            latitude: 42.6977,
+            zoom: 12
+          }}
+          style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}
+          mapStyle="mapbox://styles/mapbox/streets-v9"
+          mapboxAccessToken="pk.eyJ1IjoicGFuY2FrZWJveSIsImEiOiJjbGUyajU0dncxbXo3M3BwNmdkYXNwZzdlIn0.v1N4CI0aULZ7M6S12iW5Kg"
+        />
       </div>
-    </section>
+    </>
   );
 };
 
